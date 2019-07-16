8c2bef41b0a5274b6e6e5c3242ec11e8cf4d1f29
Martin Polder <martinpolder@gmail.com>
2019-07-16 18:06:49 -0400

    added function to create new buttons based on search terms

--- script.js
+++ script.js
@@ -6,8 +6,55 @@ $(document).ready(function() {
 
 
     for (i = 0; i < btnArr.length; i++) {
-    $(".btndiv").append("<button>" + btnArr[i]);
+    
+        // adding variable, a making it a button
+        var a = $("<button>");
+
+        // giving it a class
+        a.addClass("gifbtns");
+
+        // giving it a data attribute at the index i
+        a.attr("data-name", btnArr[i]);
+
+        // printing the name in the array at the index i
+        a.text(btnArr[i]);
+   
+        // appending the buttons into their div
+        $(".btndiv").append(a);
   } 
+
+
+
+// click event for when "search" is clicked
+$("#add-gif").on("click", function(event) {
+    event.preventDefault();
+  
+    // grabbing the text from the input box
+  var gifSearch = $("#gif-input").val() 
+
+   
+//   Similar button creation function located in the for loop above
+// im sure there is a way to add my queries into the array but this works just the same
+// just not as clean / efficient / dry
+  
+  // adding variable, a making it a button
+   var b = $("<button>");
+
+   // giving it a class
+   b.addClass("gifbtns");
+
+   // giving it a data attribute at the index i
+   b.attr("data-name", gifSearch);
+
+   // printing the name in the array at the index i
+   b.text(gifSearch);
+
+   // appending the buttons into their div
+   $(".btndiv").append(b);
+
+
+
+});
 
 });
 
