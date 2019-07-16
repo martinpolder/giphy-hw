9af571b6469f6c4eb31ccfd29cc8ca97d5cee8e9
Martin Polder <martinpolder@gmail.com>
2019-07-16 18:24:33 -0400

    saving progress, gonna go over activities and get right back lol

--- script.js
+++ script.js
@@ -2,59 +2,52 @@
 var btnArr = ["basketball", "spaghetti", "dog", "cat", "france", "eagle", "USA", "hamburger", "owl", "ferrari", "thor", "cricket", "salah"];
 
 // loop through the array and create a button
-$(document).ready(function() {
+$(document).ready(function () {
 
+    function buttonRender() {
+        for (i = 0; i < btnArr.length; i++) {
 
-    for (i = 0; i < btnArr.length; i++) {
-    
-        // adding variable, a making it a button
-        var a = $("<button>");
+            // adding variable, a making it a button
+            var a = $("<button>");
 
-        // giving it a class
-        a.addClass("gifbtns");
+            // giving it a class
+            a.addClass("gifbtns");
 
-        // giving it a data attribute at the index i
-        a.attr("data-name", btnArr[i]);
+            // giving it a data attribute at the index i
+            a.attr("data-name", btnArr[i]);
 
-        // printing the name in the array at the index i
-        a.text(btnArr[i]);
-   
-        // appending the buttons into their div
-        $(".btndiv").append(a);
-  } 
+            // printing the name in the array at the index i
+            a.text(btnArr[i]);
 
-
-
-// click event for when "search" is clicked
-$("#add-gif").on("click", function(event) {
-    event.preventDefault();
-  
-    // grabbing the text from the input box
-  var gifSearch = $("#gif-input").val() 
-
-   
-//   Similar button creation function located in the for loop above
-// im sure there is a way to add my queries into the array but this works just the same
-// just not as clean / efficient / dry
-  
-  // adding variable, a making it a button
-   var b = $("<button>");
-
-   // giving it a class
-   b.addClass("gifbtns");
-
-   // giving it a data attribute at the index i
-   b.attr("data-name", gifSearch);
-
-   // printing the name in the array at the index i
-   b.text(gifSearch);
-
-   // appending the buttons into their div
-   $(".btndiv").append(b);
+            // appending the buttons into their div
+            $(".btndiv").append(a);
+        }
+    };
 
 
 
-});
+    // click event for when "search" is clicked
+    $("#add-gif").on("click", function (event) {
+        event.preventDefault();
 
+        // grabbing the text from the input box
+        var gifSearch = $("#gif-input").val()
+
+        console.log(gifSearch);
+
+        //   pushing the input value into the btn array
+        btnArr.push(gifSearch);
+
+        //  testing to make sure its all going in the right place :) 
+        console.log(btnArr);
+
+        //  run the loop function again to render buttons for the added search terms
+        buttonRender;
+
+
+
+    });
+
+    buttonRender;
 });
 
